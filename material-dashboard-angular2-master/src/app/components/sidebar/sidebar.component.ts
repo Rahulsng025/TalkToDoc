956f66372957fc62199a0658fc547153c8f23a86
Rahul <Rahul.sng025@gmail.com>
2020-05-05 12:47:20 +0530

    Admin-layout-module: Some files are reorganized

--- material-dashboard-angular2-master/src/app/components/sidebar/sidebar.component.ts
+++ material-dashboard-angular2-master/src/app/components/sidebar/sidebar.component.ts
@@ -4,51 +4,81 @@ declare const $: any;
 declare interface RouteInfo {
   path: string;
   title: string;
-  icon: string;
+  //  icon: string;
   class: string;
 }
 export const ROUTES: RouteInfo[] = [
-  { path: "/dashboard", title: "Dashboard", icon: "dashboard", class: "" },
-  { path: "/admin", title: "Admin", icon: "admin", class: "" },
-  { path: "/doctor-admin", title: "Doctor Admin", icon: "doctor-admin", class: "" },
+  {
+    path: "/dashboard",
+    title: "Dashboard",
+    // icon: "dashboard", 
+    class: ""
+  },
+  {
+    path: "/admin",
+    title: "Admin",
+    // icon: "admin", 
+    class: ""
+  },
+  {
+    path: "/doctor-admin",
+    title: "Doctor Admin",
+    // icon: "doctor-admin", 
+    class: ""
+  },
   {
     path: "/doctor-listing",
     title: "Doctor Listing",
-    icon: "doctor",
+    //  icon: "doctor",
     class: "",
   },
   {
     path: "/diagnostic-center",
     title: "Diagnostic Center",
-    icon: "center",
+    //   icon: "center",
     class: "",
   },
-  { path: "/home-care", title: "Home Care", icon: "center", class: "" },
+  {
+    path: "/home-care",
+    title: "Home Care",
+    // icon: "center", 
+    class: ""
+  },
   {
     path: "/medical-insurance",
     title: "Medical Insurance",
-    icon: "center",
+    // icon: "center",
     class: "",
   },
-  { path: "/user-profile", title: "User Profile", icon: "person", class: "" },
+  {
+    path: "/user-profile",
+    title: "User Profile",
+    // icon: "person", 
+    class: ""
+  },
   {
     path: "/table-list",
     title: "Table List",
-    icon: "content_paste",
+    // icon: "content_paste",
     class: "",
   },
   {
     path: "/typography",
     title: "Typography",
-    icon: "library_books",
+    //icon: "library_books",
     class: "",
   },
-  { path: "/icons", title: "Icons", icon: "bubble_chart", class: "" },
+  {
+    path: "/icons",
+    title: "Icons",
+    // icon: "bubble_chart", 
+    class: ""
+  },
 
   {
     path: "/notifications",
     title: "Notifications",
-    icon: "notifications",
+    //icon: "notifications",
     class: "",
   },
 
