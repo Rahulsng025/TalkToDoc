a8e0e599b9011481270c21cfbe32e9fa6fcf89dc
Rahul <Rahul.sng025@gmail.com>
2020-06-22 22:52:06 +0530

    pdf for book appointment is working now

--- material-dashboard-angular2-master/src/app/doctor-listing/appointment-list/appointment-details/appointment-details.component.ts
+++ material-dashboard-angular2-master/src/app/doctor-listing/appointment-list/appointment-details/appointment-details.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { AppointmentDetailService } from 'app/Services/appointment-detail.service';
 import { AppointmentDetailsModel } from "app/model/appointment_list.model";
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-appointment-details',
@@ -9,6 +10,29 @@ import { AppointmentDetailsModel } from "app/model/appointment_list.model";
 })
 export class AppointmentDetailsComponent implements OnInit {
   details: AppointmentDetailsModel;
+  nativeElement: any;
+  //Convert html file to pdf
+
+  @ViewChild('content') content: ElementRef;
+
+
+  public downloadPDF() {
+    const doc = new jsPDF();
+
+    const specialElementHandler = {
+      '#editor': function (element: any, renderer: any) {
+        return true;
+      }
+    };
+
+    const content = this.content.nativeElement;
+    doc.fromHTML(content.innerHTML, 15, 15, {
+      'width': 190,
+      'elementHandlers': specialElementHandler
+    });
+    doc.save('first.pdf');
+  }
+
 
   constructor(private appointmentDetailService: AppointmentDetailService) { }
 
