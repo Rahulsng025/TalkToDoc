809611d8796983e1ba35fc7be1dfbe8536cbed2d
Rahul <Rahul.sng025@gmail.com>
2021-01-26 19:19:43 +0530

    Get request api of contacts is enabled

--- api/routes/contact.js
+++ api/routes/contact.js
@@ -5,21 +5,21 @@ const mongoose = require('mongoose');
 const contact = require('../models/contact');
 
 //Handling incoming get request of doctor_listing.
-// router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     
-//     contact.find()
-//         .exec()
-//         .then(docs => {
-//             console.log(docs);
-//             res.status(200).json(docs);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json({
-//                 error: err
-//             });
-//         });
-// });
+    contact.find()
+        .exec()
+        .then(docs => {
+           
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            
+            res.status(500).json({
+                error: err
+            });
+        });
+});
 
 //Diagnostic_center ID
 router.get('/:contactId', (req, res, next) => {
